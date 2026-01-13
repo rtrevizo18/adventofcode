ebb678e469ea92442f9d3dad5b479057c407c874
rtrevizo18 <ricardotrevizo18@gmail.com>
2026-01-12 20:44:39 -0600

    feat: Add starter code for jan-12-2026

---
+++ jan-12-2026/jan-12-2026.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Goal : number of times the dial is
+// left pointing at 0 after any rotation in the sequence
+
+int main() {
+
+  FILE *fptr;
+
+  fptr = fopen("input.txt", "r");
+}
